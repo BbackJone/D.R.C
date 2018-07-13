294e28ce84e8281d0e2a1534a413a48e5804128f
Woohyun Shin <cpu344@gmail.com>
2018-07-13 17:40:00 +0900

    Changed touch input so short or stationary touch causes weapon to fire
    and fire button is removed.
    not tested on a real device - must be tuned later.

--- Assets/Script/PlayerInputTouch.cs
+++ Assets/Script/PlayerInputTouch.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class PlayerInputTouch : MonoBehaviour {
     public ControlStickScript controlStick;
     private PlayerData data;
+    private PlayerAction action;
     
     private float stickSensitivity = 2f;
     private float rotateXSensitivity = 0.24f;
@@ -18,15 +19,25 @@ public class PlayerInputTouch : MonoBehaviour {
 
     private float mouseX;
     private float mouseY;
+
+    private int screenDraggingPointer = -1;
+    // scale threshold according to the screen resolution - base value is 50 at 720p
+    private float touchRotateThreshold = 50 * (Screen.width / 1280);
+    private const float touchFireTimeConst = 0.33f;
+    private float touchFireTime = touchFireTimeConst;
+    private Dictionary<int, Vector2> touchDownPos;
     
 	void Start () {
         if (controlStick == null) enabled = false;
         data = GetComponent<PlayerData>();
+        action = GetComponent<PlayerAction>();
         lookY = data.m_Camera.transform.eulerAngles.y;
 
         mouseX = Input.mousePosition.x;
         mouseY = Input.mousePosition.y;
-    }
+
+        touchDownPos = new Dictionary<int, Vector2>();
+}
 
     void Update() {
         // Move character
@@ -39,20 +50,52 @@ public class PlayerInputTouch : MonoBehaviour {
         if (Input.touchCount > 0) {
             // for every active touch pointer...
             for (int i = 0; i < Input.touchCount; i++) {
+                Touch touch = Input.GetTouch(i);
                 // if this touch pointer is not using control stick...
-                if (i != controlStick.draggingPointer) {
-                    // assume this touch is trying to rotate camera
-                    Touch touch = Input.GetTouch(i);
+                if (touch.fingerId != controlStick.draggingPointer) {
+                    // if screenDraggingPointer is not set or this touch pointer is screenDraggingPointer...
+                    if (screenDraggingPointer == -1 || touch.fingerId == screenDraggingPointer) { 
+                        if (touch.phase == TouchPhase.Began) {
+                            // save touch start position
+                            screenDraggingPointer = touch.fingerId;
+                            touchDownPos.Add(touch.fingerId, touch.position);
+                        } else if (touch.phase == TouchPhase.Ended) {
+                            // fire once if touchFireTime is not expired (if short touch has happened)
+                            if (touchFireTime > 0f) {
+                                action.Firebullet();
+                            }
+                            // reset touch state
+                            screenDraggingPointer = -1;
+                            touchDownPos.Remove(touch.fingerId);
+                            touchFireTime = touchFireTimeConst;
+                        } else if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+                            if (touchDownPos[touch.fingerId] != null) {
+                                // check if touch point has moved beyond threshold
+                                if (Vector2.Distance(touchDownPos[touch.fingerId], touch.position) > 30) {
+                                    // user is dragging, set touchDownPos[touch.fingerId] to null to indicate this finger is rotating screen
+                                    touchDownPos.Remove(touch.fingerId);
+                                } else {
+                                    // touch point is staying inside threshold - check touchFireTime
+                                    if (touchFireTime < 0f) {
+                                        // if touchFireTime is under 0, fire the weapon
+                                        // since this call is in update, firing will called continuously
+                                        action.Firebullet();
+                                    } else {
+                                        // decrease touchFireTime by deltaTime
+                                        touchFireTime -= Time.deltaTime;
+                                    }
+                                }
+                            } else {
+                                // user is clearly trying to rotate screen
+                                transform.Rotate(Vector3.up * touch.deltaPosition.x * rotateXSensitivity);
+                                lookY -= touch.deltaPosition.y * rotateYSensitivity;
+                                lookY = Mathf.Clamp(lookY, -80f, 50f);
+                                data.m_Camera.transform.eulerAngles = new Vector3(lookY, data.m_Camera.transform.eulerAngles.y, 0f);
+                            }
+                        }
 
-                    if (touch.phase != TouchPhase.Began) {
-                        // apply rotation
-                        transform.Rotate(Vector3.up * touch.deltaPosition.x * rotateXSensitivity);
-                        lookY -= touch.deltaPosition.y * rotateYSensitivity;
-                        lookY = Mathf.Clamp(lookY, -80f, 50f);
-                        data.m_Camera.transform.eulerAngles = new Vector3(lookY, data.m_Camera.transform.eulerAngles.y, 0f);
+                        break;
                     }
-
-                    break;
                 }
             }
         }
