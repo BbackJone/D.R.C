9e43b1ac1ee74945574a98915cd7a78dd4c01af0
Woohyun Shin <cpu344@gmail.com>
2018-07-20 14:04:31 +0900

    Added zombie hitbox script

---
+++ Assets/Script/AttackHitboxScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackHitboxScript : MonoBehaviour {
+
+    private PlayerData data;
+
+    private void Start()
+    {
+        data = GameObject.Find("Santa").GetComponent<PlayerData>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Santa"))
+        {
+            data.m_Hp = Math.Max(data.m_Hp - 1, 0);
+        }
+    }
+}
