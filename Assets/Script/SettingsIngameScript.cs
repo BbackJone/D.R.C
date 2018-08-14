8e69eb099c2558986e33cafb6ca8b176011854e3
Woohyun Shin <cpu344@gmail.com>
2018-08-14 20:24:18 +0900

    Added sensitivity slider
    All sliders will remember its value through PlayerPrefs
    Sensitivity slider is fully implemented within PlayerInputTouch
    Volume slider still need some work

--- Assets/Script/SettingsIngameScript.cs
+++ Assets/Script/SettingsIngameScript.cs
@@ -10,9 +10,33 @@ public class SettingsIngameScript : MonoBehaviour {
 
     public Slider musicVolumeSlider;
     public Slider seVolumeSlider;
-    
+    public Slider touchSensitivitySlider;
+    public PlayerInputTouch touchInput;
+
+    float musicVol;
+    float seVol;
+    float sensitivity;
+
     // Possible problem: Attack hitbox collision may continue to occur after the game paused
     // └Workaround: other scripts can use (Time.timeScale == 1f) to determine if the game is paused or not
+
+    void Update() {
+        if (musicVolumeSlider.value != musicVol) {
+            PlayerPrefs.SetFloat("musicvol", musicVolumeSlider.value);
+            musicVol = musicVolumeSlider.value;
+        }
+        if (seVolumeSlider.value != seVol) {
+            PlayerPrefs.SetFloat("sevol", seVolumeSlider.value);
+            seVol = seVolumeSlider.value;
+        }
+        if (touchSensitivitySlider.value != sensitivity) {
+            PlayerPrefs.SetFloat("sensitivity", touchSensitivitySlider.value);
+            sensitivity = touchSensitivitySlider.value;
+            if (touchInput != null) {
+                touchInput.rotateUserSensitivity = (sensitivity * 1.8f) + 0.2f;
+            }
+        }
+    }
 
     void OnEnable() {
         stageMgr = GameObject.Find("StageMgr").GetComponent<StageMgr>();
@@ -21,7 +45,12 @@ public class SettingsIngameScript : MonoBehaviour {
     }
 
     private void ShowSettings() {
-        // TODO: Update settings here...
+        musicVol = PlayerPrefs.GetFloat("musicvol", 1f);
+        seVol = PlayerPrefs.GetFloat("sevol", 1f);
+        sensitivity = PlayerPrefs.GetFloat("sensitivity", 0.5f);
+        musicVolumeSlider.value = musicVol;
+        seVolumeSlider.value = seVol;
+        touchSensitivitySlider.value = sensitivity;
         gameObject.SetActive(true);
     }
 
--- Assets/Script/SettingsTitleScript.cs
+++ Assets/Script/SettingsTitleScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,9 +7,38 @@ using UnityEngine.UI;
 public class SettingsTitleScript : MonoBehaviour {
     public Slider musicVolumeSlider;
     public Slider seVolumeSlider;
-    
+    public Slider touchSensitivitySlider;
+    public PlayerInputTouch touchInput;
+
+    float musicVol;
+    float seVol;
+    float sensitivity;
+
+    void Update() {
+        if (musicVolumeSlider.value != musicVol) {
+            PlayerPrefs.SetFloat("musicvol", musicVolumeSlider.value);
+            musicVol = musicVolumeSlider.value;
+        }
+        if (seVolumeSlider.value != seVol) {
+            PlayerPrefs.SetFloat("sevol", seVolumeSlider.value);
+            seVol = seVolumeSlider.value;
+        }
+        if (touchSensitivitySlider.value != sensitivity) {
+            PlayerPrefs.SetFloat("sensitivity", touchSensitivitySlider.value);
+            sensitivity = touchSensitivitySlider.value;
+            if (touchInput != null) {
+                touchInput.rotateUserSensitivity = (sensitivity * 1.8f) + 0.2f;
+            }
+        }
+    }
+
     private void ShowSettings() {
-        // TODO: Update settings here...
+        musicVol = PlayerPrefs.GetFloat("musicvol", 1f);
+        seVol = PlayerPrefs.GetFloat("sevol", 1f);
+        sensitivity = PlayerPrefs.GetFloat("sensitivity", 0.5f);
+        musicVolumeSlider.value = musicVol;
+        seVolumeSlider.value = seVol;
+        touchSensitivitySlider.value = sensitivity;
         gameObject.SetActive(true);
     }
 
