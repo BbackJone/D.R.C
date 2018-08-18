88770fa439d47bad24d18360f1f2f5c32a1897f1
Woohyun Shin <cpu344@gmail.com>
2018-08-18 12:16:07 +0900

    Basic multilanguage implementation

---
+++ Assets/Script/LanguageManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageManager {
+    private LanguageManager instance;
+
+    private LanguageManager()
+    {
+        // TODO: load database here...
+    }
+
+    public LanguageManager GetInstance()
+    {
+        if (instance != null) instance = new LanguageManager();
+
+        return instance;
+    }
+}
