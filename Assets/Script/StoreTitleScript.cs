8f02696cd850fbc245d743cf63e7fc0547088d5a
Woohyun Shin <cpu344@gmail.com>
2018-08-18 17:29:12 +0900

    Basic store implementation

---
+++ Assets/Script/StoreTitleScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreTitleScript : MonoBehaviour {
+
+    public void ShowStore()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void HideStore()
+    {
+        gameObject.SetActive(false);
+    }
+}
