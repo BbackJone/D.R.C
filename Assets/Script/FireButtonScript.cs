47fc4a3f8cb1dc04fef8510b1c1cc082ed97f082
Woohyun Shin <cpu344@gmail.com>
2018-08-25 19:41:28 +0900

    Changed weapon fire mechanism
    Added top-left single fire
    Changed fire button to be toggled on autofire weapon

--- Assets/Script/FireButtonScript.cs
+++ Assets/Script/FireButtonScript.cs
@@ -3,22 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class FireButtonScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+public class FireButtonScript : MonoBehaviour, IPointerDownHandler {
     private PlayerData playerData;
+    private Image image;
+    private Image inImage;
+    private float imageColor = 1f;
+    private const float imageColorBright = 1f;
+    private const float imageColorDark = 0.4f;
+    private bool autofireByFireButton = false;
 
     void Start() {
         playerData = ObjectManager.m_Inst.Objects.m_Playerlist[0].GetComponent<PlayerData>();
         if (playerData == null)
             enabled = false;
+
+        image = GetComponent<Image>();
+        inImage = transform.Find("Image").gameObject.GetComponent<Image>();
     }
 
     public void OnPointerDown(PointerEventData eventData) {
-        playerData.m_isShooting = true;
+        if (playerData.m_WeaponInhand.m_Autoshot)
+        {
+            playerData.m_isShooting = !playerData.m_isShooting;
+            autofireByFireButton = playerData.m_isShooting;
+            imageColor = playerData.m_isShooting ? imageColorDark : imageColorBright;
+            image.color = new Color(imageColor, imageColor, imageColor);
+            inImage.color = new Color(imageColor, imageColor, imageColor);
+        }
+        else
+        {
+            StartCoroutine(SingleShot());
+        }
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+    IEnumerator SingleShot()
     {
+        playerData.m_isShooting = true;
+        yield return new WaitForEndOfFrame();
         playerData.m_isShooting = false;
+    }
+
+    public void StopAutofireByFireButton()
+    {
+        if (autofireByFireButton)
+        {
+            playerData.m_isShooting = false;
+            autofireByFireButton = false;
+            imageColor = imageColorBright;
+            image.color = new Color(imageColor, imageColor, imageColor);
+            inImage.color = new Color(imageColor, imageColor, imageColor);
+        }
     }
 }
