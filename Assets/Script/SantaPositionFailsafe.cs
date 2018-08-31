436daa422dc9ff9f88c3dc6a3450436bd96c459f
Woohyun Shin <cpu344@gmail.com>
2018-08-31 15:53:13 +0900

    Added SantaPositionFailsafe

---
+++ Assets/Script/SantaPositionFailsafe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaPositionFailsafe : MonoBehaviour {
+    public Transform santaPos;
+
+	void Start () {
+        if (santaPos == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        StartCoroutine(CheckSantaPos());
+	}
+	
+	IEnumerator CheckSantaPos() {
+        while (true)
+        {
+            if (santaPos.position.y < -30) santaPos.position = transform.position;
+            yield return new WaitForSeconds(5f);
+        }
+	}
+}
