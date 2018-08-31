8e74547ee3ca8be3a0da4b806a4f338d44ceb53b
Woohyun Shin <cpu344@gmail.com>
2018-08-31 12:09:04 +0900

    Turret spawn implementation

---
+++ Assets/Script/TurretBodyScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBodyScript : MonoBehaviour {
+
+    public void SetPositionAndEnable(Transform santaPos)
+    {
+        transform.position = santaPos.position + (santaPos.forward * 3f);
+        gameObject.SetActive(true);
+    }
+}
