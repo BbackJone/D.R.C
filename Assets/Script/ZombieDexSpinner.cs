47ca96232bc4b0049abbef75171f69621fd8961f
Woohyun Shin <cpu344@gmail.com>
2018-09-06 23:52:01 +0900

    ZombieDex work in progress

---
+++ Assets/Script/ZombieDexSpinner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieDexSpinner : MonoBehaviour {
+    public Transform camPos;
+
+	void FixedUpdate () {
+        transform.Rotate(Vector3.up * 0.5f);
+	}
+}
